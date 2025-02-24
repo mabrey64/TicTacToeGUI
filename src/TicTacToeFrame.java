a613494df11d55bf361a929e9002867d89aa7c52
mabreydn <mabreydn@mail.uc.edu>
2025-02-24 14:22:22 -0500

    Forgotten Code
    Forgot to add in code for exit messages.

--- src/TicTacToeFrame.java
+++ src/TicTacToeFrame.java
@@ -137,6 +137,7 @@ public class TicTacToeFrame extends JFrame
                 game();
             }
             else {
+                JOptionPane.showMessageDialog(this, "The user has exited the game", "Exit", JOptionPane.INFORMATION_MESSAGE);
                 System.exit(0);
             }
         } else if (moveCount >= MovesForTie && isTie()) {
@@ -149,6 +150,7 @@ public class TicTacToeFrame extends JFrame
                 game();
             }
             else {
+                JOptionPane.showMessageDialog(this, "The user has exited the game", "Exit", JOptionPane.INFORMATION_MESSAGE);
                 System.exit(0);
             }
         }
